4296fe51f2172af84a8a6cef2fb50939574697b2
Druva4444 <druvadruvs@gmail.com>
2025-06-23 13:29:49 +0530

    Fix routing for Vercel SPA

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ import cors from 'cors'
 import bodyParser from 'body-parser';
 dotenv.config();
 const app = express();
-app.use(cors({origin:process.env.CLIENT_ORIGIN ||"http://localhost:5173",credentials:true}))
+app.use(cors({origin:process.env.FRONTEND_URL ||"http://localhost:5173",credentials:true}))
 app.use('/api/',webhookrouter)
 app.use(express.json())
 app.use(cookieParser())
